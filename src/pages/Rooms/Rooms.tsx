bdd7389435a7cd1362a50f1b7ef85536321a75c7
Bartosz <bartoszmrosek@op.pl>
2023-04-21 20:46:04 +0200

    Responsive design for rooms

--- src/pages/Rooms/Rooms.tsx
+++ src/pages/Rooms/Rooms.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { StyledMainWrapper } from "../../components/StyledMainWrapper/StyledMainWrapper";
 import { GoBackButton } from "../../components/Overlays/GoBackButton/GoBackButton";
 import styles from "./Rooms.module.css";
+import { useAppSelector } from "../../hooks/reduxHooks";
 
 const Rooms: React.FC = () => {
+    const isUserLogged = useAppSelector(state => state.onlineUser.id) !== -1;
     return (
         <StyledMainWrapper classNames={styles.roomsWrapper}>
             <h1 className={styles.tableCaption}>All current game rooms</h1>
@@ -14,18 +16,48 @@ const Rooms: React.FC = () => {
                             <tr>
                                 <th className={styles.tableTh}>Game id</th>
                                 <th className={styles.tableTh}>No. of players</th>
-                                <th className={styles.tableTh} />
+                                {isUserLogged && (
+                                    <th className={`${styles.tableTh} ${styles.refreshTh}`}>
+                                        <button className={styles.refreshBtn}>
+                                            <img
+                                                src="/Graphics/refresh.svg"
+                                            />
+                                        </button>
+                                    </th>
+                                )}
+
                             </tr>
                         </thead>
                         <tbody>
+                            <tr className={styles.mobileOptions}>
+                                <td className={styles.begginingTableTd}>
+                                    <button className={styles.inGameTableBtn}>Refresh</button>
+                                </td>
+                                {isUserLogged && (
+                                    <td className={styles.begginingTableTd}>
+                                        <button className={styles.inGameTableBtn}>Create & Join</button>
+                                    </td>
+                                )}
+                            </tr>
                             {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].map((el) => (
                                 <tr key={el}>
                                     <td data-cell="Game id: " className={styles.tableTd}>ABCD</td>
                                     <td data-cell="No. of players: " className={styles.tableTd}>2/5</td>
-                                    <td className={styles.tableTd}>Placeholder</td>
+                                    {isUserLogged && (
+                                        <td className={styles.tableTd}>
+                                            <button className={styles.joinGameBtn}>Join</button>
+                                        </td>
+                                    )}
                                 </tr>
                             ))
                     }
+                            {isUserLogged && (
+                                <tr className={styles.createGameEnd}>
+                                    <td colSpan={3}>
+                                        <button className={styles.inGameTableBtn}>Create & join game</button>
+                                    </td>
+                                </tr>
+                            )}
                         </tbody>
                     </table>
                 </div>
